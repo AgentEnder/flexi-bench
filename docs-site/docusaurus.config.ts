c0741420232e3e81588670af17cf267267c83dd7
Craigory Coppola <craigorycoppola@gmail.com>
2024-07-16 20:28:59 -0400

    feat(repo): add ability to publish only docs

--- docs-site/docusaurus.config.ts
+++ docs-site/docusaurus.config.ts
@@ -20,6 +20,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'agentender', // Usually your GitHub org/user name.
   projectName: 'flexi-bench', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
